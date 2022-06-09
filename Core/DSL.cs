84a9938ec52cb9a41fdca93e718f24bc62fac8f2
Rickartus <Rickartus@DESKTOP-71A5QU8>
2022-06-09 19:21:51 -0300

    update novo valida download

--- Core/DSL.cs
+++ Core/DSL.cs
@@ -13,27 +13,18 @@ namespace BaseProjectAutomation.Core
     {
         // Funções de interação ------------------------
         public void Wait(int time) => Thread.Sleep(time);
-
+        public void ClearData(string element) => driver.FindElement(By.XPath(element)).Clear();
+        public void ClickOut() => driver.FindElement(By.XPath("//html")).Click();
+        public string EndFile() { if (!testPassed) { Assert.Fail(); } return testOk; }
         public void WaitElement(string element)
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(90));
             Wait(1000); wait.Until((d) => { return d.FindElement(By.XPath(element)); }); Wait(2000);
         }
-
         public void WaitElementGone(string element)
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(90));
             Wait(1000); wait.Until(d => d.FindElements(By.XPath(element)).Count == 0); Wait(2000);
-        }
-
-        public void ClearData(string element) => driver.FindElement(By.XPath(element)).Clear();
-
-        public void ClickOut() => driver.FindElement(By.XPath("//html")).Click();
-
-        public string EndFile()
-        {
-            if (!testPassed) { Assert.Fail(); }
-            return testOk;
         }
 
 
@@ -171,7 +162,7 @@ namespace BaseProjectAutomation.Core
 
 
         // Funções de interação --------------------------------------------------
-        public string ClicaElemento(string element, string msgOk, string msgError, int wait = 1000, string loading = null)
+        public string ClicaElemento(string element, [Optional] string msgOk, [Optional] string msgError, int wait = 1000, string loading = null)
         {
             if (!testPassed) Assert.Fail();
             try
@@ -186,7 +177,7 @@ namespace BaseProjectAutomation.Core
                 return "<font color = red>" + msgError + testNok;
             }
         }
-        public string EscreveTexto(string element, string value, string msgOk, string msgError)
+        public string EscreveTexto(string element, string value, [Optional] string msgOk, [Optional] string msgError)
         {
             if (!testPassed) Assert.Fail();
             try
@@ -200,12 +191,12 @@ namespace BaseProjectAutomation.Core
                 return "<font color = red>" + msgError + testNok;
             }
         }
-        public string ValidaDados(string element, string value, [Optional] string msgOk, [Optional] string msgError)
+        public string ValidaDados(string xPath, string value, [Optional] string msgOk, [Optional] string msgError)
         {
             if (!testPassed) Assert.Fail();
             try
             {
-                Assert.IsTrue(driver.FindElement(By.XPath(element)).Text.Contains(value));
+                Assert.IsTrue(driver.FindElement(By.XPath(xPath)).Text.Contains(value));
                 return msgOk + "<br>";
             }
             catch
@@ -214,13 +205,13 @@ namespace BaseProjectAutomation.Core
                 return "<font color = red>" + msgError + testNok;
             }
         }
-        public string MenuDropDown(string element, string value, [Optional] string msgOk, [Optional] string msgError)
+        public string MenuDropDown(string xPath1, string xPath2, [Optional] string msgOk, [Optional] string msgError)
         {
             if (!testPassed) Assert.Fail();
             try
             {
-                driver.FindElement(By.XPath(element)).Click(); Wait(500);
-                driver.FindElement(By.XPath(value)).Click(); Wait(750);
+                driver.FindElement(By.XPath(xPath1)).Click(); Wait(500);
+                driver.FindElement(By.XPath(xPath2)).Click(); Wait(750);
                 return msgOk + "<br>";
             }
             catch
@@ -229,7 +220,7 @@ namespace BaseProjectAutomation.Core
                 return "<font color = red>" + msgError + testNok;
             }
         }
-        public string UploadArquivo(string input, string path, string msgOk, string msgError)
+        public string UploadArquivo(string input, string path, [Optional] string msgOk, [Optional] string msgError)
         {
             if (!testPassed) Assert.Fail();
             try
@@ -243,27 +234,33 @@ namespace BaseProjectAutomation.Core
                 return "<font color = red>" + msgError + testNok;
             }
         }
-        public string DownloadArquivo(string fileName, string msgOk, string msgError)
+        public string ValidaDownload(string partialFileName, [Optional] string msgOk, [Optional] string msgError)
         {
             if (!testPassed) Assert.Fail();
             try
             {
-                string downloadsPath = Environment.GetEnvironmentVariable("USERPROFILE") + @"\Downloads\";
-                string downloadedFile = downloadsPath + fileName;
-                for (int i = 0; i < 30; i++)
+                DirectoryInfo downloadsPath = new DirectoryInfo(Environment.GetEnvironmentVariable("USERPROFILE") + @"\Downloads\");
+                FileInfo[] filesInFolder = downloadsPath.GetFiles(partialFileName + "*.*");
+
+                foreach (FileInfo foundFile in filesInFolder)
                 {
-                    if (File.Exists(downloadedFile)) { break; }
-                    Wait(1000);
+                    string fullFileName = foundFile.FullName;
+
+                    for (int i = 0; i < 30; i++)
+                    {
+                        if (File.Exists(fullFileName)) { break; }
+                        Wait(1000);
+                    }
+                    var length = new FileInfo(fullFileName).Length;
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Wait(1000);
+                        var newLength = new FileInfo(fullFileName).Length;
+                        if (newLength == length && length != 0) { break; }
+                        length = newLength;
+                    }
+                    File.Delete(fullFileName);
                 }
-                var length = new FileInfo(downloadedFile).Length;
-                for (int i = 0; i < 60; i++)
-                {
-                    Wait(1000);
-                    var newLength = new FileInfo(downloadedFile).Length;
-                    if (newLength == length && length != 0) { break; }
-                    length = newLength;
-                }
-                File.Delete(downloadedFile);
                 return msgOk + "<br>";
             }
             catch
