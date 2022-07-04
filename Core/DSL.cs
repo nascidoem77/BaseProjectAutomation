c7a28711753e5291e1e59e999ccf735cde5cac4c
Rickartus <Rickartus@DESKTOP-71A5QU8>
2022-07-03 22:14:18 -0300

    update dsl

--- Core/DSL.cs
+++ Core/DSL.cs
@@ -4,7 +4,6 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace BaseProjectAutomation.Core
